d181fcb63516da40a415dcaabf73585f8a5c25f3
Anthony Carothers <Bobguy1206@gmail.com>
2025-11-06 19:01:30 -0500

    new

---
+++ My project/Assets/scripts/Scene_Manager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+public class Scene_Manager : MonoBehaviour
+{
+    public void loadlevel1()
+    {
+        SceneManager.LoadSceneAsync("Scene1");
+    }
+
+
+
+
+
+}
